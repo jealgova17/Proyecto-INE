5f41fcc21d1886bdb09d6586874bb832dc56add5
mapache06 <301710709@bis.ute.edu.mx>
2019-04-04 14:52:16 -0600

    Se agregaron graficas

--- Proyecto-INE/Proyecto-INE/Models/INEDbContext.cs
+++ Proyecto-INE/Proyecto-INE/Models/INEDbContext.cs
@@ -19,7 +19,7 @@ namespace Proyecto_INE.Models
         public DbSet<Municipio> Municipios { get; set; }
         public DbSet<Estado> Estados { get; set; }
         public DbSet<Persona> Personas { get; set; }
-        public DbSet<PartidoPolitico> PartidoPoliticos{get; set;}
+        public DbSet<PartidoPolitico> PartidoPoliticos {get; set;}
         public DbSet<Puesto> Puestos { get; set; }
         public DbSet<Voto> Votos { get; set; }
 
